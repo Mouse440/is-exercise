aef36169aa6935c15469758e014aabbf982de08f
mouse440 <blinkblinklink@aim.com>
2016-03-17 07:00:04 +0000

    improved the code to use angular to control meny

--- js/controller.js
+++ js/controller.js
@@ -5,6 +5,11 @@ controllers.
         var path = ['data/',$routeParams.routeName,'.json'].join(''); //construct the route path
         $http.get(path).success(function(data){
     		$scope.content = data;
-    		$scope.routeName = $routeParams.routeName;
+    		$scope.$emit('dataLoaded',{ routeName: $routeParams.routeName}); //pass route name up the chain
     	});
+    }]).
+    controller('MenuController', ['$scope','$http', '$routeParams', function($scope,$http,$routeParams){
+        $scope.$on('dataLoaded', function(event, args){     
+            $scope.routeName = args.routeName;                        //set routename to the scope 
+          });
     }]);
